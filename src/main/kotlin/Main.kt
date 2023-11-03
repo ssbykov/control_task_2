8fb632038bc384a62cd2e68396b039b042b66b80
Sergei Bykov <ss.bykoff@gmail.com>
2023-11-03 12:58:07 +0300

    убрал return

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,12 +7,8 @@ fun main() {
 
 }
 
-fun calcCommission(cardType: String = "VK Pay", initSum: Double = 0.0, amount: Double): Double {
-    val commission: Double =
-        when (cardType) {
-            "MasterCard", "Maestro" -> if (initSum >= 75000.0) amount * 0.006 + 20.0 else 0.0
-            "Visa", "Мир" -> if (amount * 0.0075 < 35.0) 35.0 else amount * 0.0075
-            else -> 0.0
-        }
-    return commission
+fun calcCommission(cardType: String = "VK Pay", initSum: Double = 0.0, amount: Double) = when (cardType) {
+    "MasterCard", "Maestro" -> if (initSum >= 75000.0) amount * 0.006 + 20.0 else 0.0
+    "Visa", "Мир" -> if (amount * 0.0075 < 35.0) 35.0 else amount * 0.0075
+    else -> 0.0
 }
